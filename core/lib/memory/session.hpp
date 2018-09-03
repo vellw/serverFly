609956b028ae595d11438956144f45cef04fb1fb
vellw <vellw1002@gmail.com>
2018-09-03 18:14:05 +0800

    更新session（最近在构思如何处理好session问题）

--- core/lib/memory/session.hpp
+++ core/lib/memory/session.hpp
@@ -5,10 +5,22 @@
 #ifndef SERVERFLY_SESSION_HPP
 #define SERVERFLY_SESSION_HPP
 
+#include <string>
+
 namespace http::server
 {
-    class session {
+    class session
+    {
+    public:
+        explicit session() = default;
 
+    public:
+        bool conf(std::string _conf_str);
+        bool conf(std::string key, std::string value);
+
+    public:
+        bool set(std::string key, std::string value);
+        bool set(const char* key, std::string value);
     };
 }
 
